f7ada3a417701bd9f8c1c83dcbedff57e66640ab
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2021-05-22 17:54:57 -0400

    v2.5.0 to add support to WiFi101
    ### Major Release v2.5.0
    
    1. Add support to SAMD_MKR1000 and SAMD_MKRWIFI1010 using WiFi101.  To be used with [**Forked WiFi101** library](https://github.com/khoih-prog/WiFi101)
    2. Clean compiler's warnings
    3. Add examples for WiFi101

---
+++ examples/WiFi101/WebSocketClientSocketIO_WiFi101/WebSocketClientSocketIO_WiFi101.ino
@@ -0,0 +1,242 @@
+/****************************************************************************************************************************
+  WebSocketClientSocketIO_NINA.ino
+  For boards using WiFiNINA Shield/Module
+
+  Blynk_WiFiNINA_WM is a library for the Mega, Teensy, SAM DUE, nRF52, STM32 and SAMD boards
+  (https://github.com/khoih-prog/Blynk_WiFiNINA_WM) to enable easy configuration/reconfiguration and
+  autoconnect/autoreconnect of WiFiNINA/Blynk
+  
+  Based on and modified from WebSockets libarary https://github.com/Links2004/arduinoWebSockets
+  to support other boards such as  SAMD21, SAMD51, Adafruit's nRF52 boards, etc.
+  
+  Built by Khoi Hoang https://github.com/khoih-prog/WebSockets_Generic
+  Licensed under MIT license
+
+  Created on: 06.06.2016
+  Author: Markus Sattler
+ *****************************************************************************************************************************/
+
+#if !( defined(ARDUINO_SAMD_MKR1000) || defined(ARDUINO_SAMD_MKRWIFI1010) )
+  #error This code is intended to run on the SAMD_MKR1000 or SAMD_MKRWIFI1010 using WiFi101! Please check your Tools->Board setting.
+#endif
+
+#ifndef BOARD_NAME
+  #if defined(ARDUINO_SAMD_MKR1000)
+    #define BOARD_NAME    "ARDUINO_SAMD_MKR1000"
+  #elif defined(ARDUINO_SAMD_MKRWIFI1010)
+    #define BOARD_NAME    "ARDUINO_SAMD_MKRWIFI1010"
+  #else
+    #define BOARD_NAME    BOARD_TYPE
+  #endif  
+#endif
+
+#define _WEBSOCKETS_LOGLEVEL_     4
+
+#define WEBSOCKETS_NETWORK_TYPE   NETWORK_WIFI101
+
+#include <WiFi101.h>
+#include <driver/source/nmasic.h>
+
+#include <ArduinoJson.h>
+
+#include <WebSocketsClient_Generic.h>
+#include <SocketIOclient_Generic.h>
+
+SocketIOclient socketIO;
+
+int status = WL_IDLE_STATUS;
+
+// Select the IP address according to your local network
+IPAddress clientIP(192, 168, 2, 225);
+
+// Select the IP address according to your local network
+IPAddress serverIP(192, 168, 2, 51);
+uint16_t  serverPort = 3000;
+
+//IPAddress serverIP(10, 11, 100, 100);
+//uint16_t  serverPort = 8880;
+
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+
+char ssid[] = "your_ssid";        // your network SSID (name)
+char pass[] = "12345678";    // your network password (use for WPA, or use as key for WEP), length must be 8+
+
+void socketIOEvent(socketIOmessageType_t type, uint8_t * payload, size_t length) 
+{
+  switch (type) 
+  {
+    case sIOtype_DISCONNECT:
+      Serial.println("[IOc] Disconnected");
+      break;
+    case sIOtype_CONNECT:
+      Serial.print("[IOc] Connected to url: ");
+      Serial.println((char*) payload);
+      
+      break;
+    case sIOtype_EVENT:
+      Serial.print("[IOc] Get event: ");
+      Serial.println((char*) payload);
+      
+      break;
+    case sIOtype_ACK:
+      Serial.print("[IOc] Get ack: ");
+      Serial.println(length);
+      
+      //hexdump(payload, length);
+      break;
+    case sIOtype_ERROR:
+      Serial.print("[IOc] Get error: ");
+      Serial.println(length);
+      
+      //hexdump(payload, length);
+      break;
+    case sIOtype_BINARY_EVENT:
+      Serial.print("[IOc] Get binary: ");
+      Serial.println(length);
+      
+      //hexdump(payload, length);
+      break;
+    case sIOtype_BINARY_ACK:
+       Serial.print("[IOc] Get binary ack: ");
+      Serial.println(length);
+      
+      //hexdump(payload, length);
+      break;
+      
+    default:
+      break;  
+  }
+}
+
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("WebSockets Client IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void setup()
+{
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.println("\nStart WebSocketClientSocketIO_NINA on " + String(BOARD_NAME));
+  Serial.println(WEBSOCKETS_GENERIC_VERSION);
+
+  Serial.println("Used/default SPI pinout:");
+  Serial.print("MOSI:");
+  Serial.println(MOSI);
+  Serial.print("MISO:");
+  Serial.println(MISO);
+  Serial.print("SCK:");
+  Serial.println(SCK);
+  Serial.print("SS:");
+  Serial.println(SS);
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_SHIELD) 
+  {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    return;
+  }
+
+  String fv = WiFi.firmwareVersion();
+  Serial.print("Firmware version installed: ");
+  Serial.println(fv);
+
+  String latestFv;
+    
+  if (REV(GET_CHIPID()) >= REV_3A0) 
+  {
+    // model B
+    latestFv = WIFI_FIRMWARE_LATEST_MODEL_B;
+  } 
+  else 
+  {
+    // model A
+    latestFv = WIFI_FIRMWARE_LATEST_MODEL_A;
+  }
+  
+  if (fv < latestFv) 
+  {
+    Serial.println("Please upgrade the firmware");
+    // Print required firmware version
+    Serial.print("Latest firmware version available : ");
+    Serial.println(latestFv);
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    //delay(10000);
+  }
+
+  printWifiStatus();
+
+  // server address, port and URL
+  Serial.print("Connecting to WebSockets Server @ IP address: ");
+  Serial.print(serverIP);
+  Serial.print(", port: ");
+  Serial.println(serverPort);
+
+  // server address, port and URL
+  socketIO.begin(serverIP, serverPort);
+
+  // event handler
+  socketIO.onEvent(socketIOEvent);
+}
+
+unsigned long messageTimestamp = 0;
+
+void loop() 
+{
+  socketIO.loop();
+
+  uint64_t now = millis();
+
+  if (now - messageTimestamp > 2000) 
+  {
+    messageTimestamp = now;
+
+    // creat JSON message for Socket.IO (event)
+    DynamicJsonDocument doc(1024);
+    JsonArray array = doc.to<JsonArray>();
+
+    // add evnet name
+    // Hint: socket.on('event_name', ....
+    array.add("event_name");
+
+    // add payload (parameters) for the event
+    JsonObject param1 = array.createNestedObject();
+    param1["now"]     = (uint32_t) now;
+
+    // JSON to String (serializion)
+    String output;
+    serializeJson(doc, output);
+
+    // Send event
+    socketIO.sendEVENT(output);
+
+    // Print JSON for debugging
+    Serial.println(output);
+  }
+}
