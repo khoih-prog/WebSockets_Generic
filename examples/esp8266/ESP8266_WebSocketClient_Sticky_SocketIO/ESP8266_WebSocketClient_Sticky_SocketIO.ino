2952e554513dc51a660f349f459b3e3bd818b53c
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2021-12-12 01:56:01 -0500

    v2.11.1 to add sticky-session SIO
    ### Release v2.11.1
    
    1. Add option to use `transport=websocket` with sticky-session SIO server. Check [Run websocket only mode for Socket.IO #16](https://github.com/khoih-prog/WebSockets_Generic/issues/16)
    2. Add some Sticky_SIO-related examples

---
+++ examples/esp8266/ESP8266_WebSocketClient_Sticky_SocketIO/ESP8266_WebSocketClient_Sticky_SocketIO.ino
@@ -0,0 +1,177 @@
+/****************************************************************************************************************************
+  ESP8266_WebSocketClient_Sticky_SocketIO.ino
+  For ESP8266
+
+  Based on and modified from WebSockets libarary https://github.com/Links2004/arduinoWebSockets
+  to support other boards such as  SAMD21, SAMD51, Adafruit's nRF52 boards, etc.
+
+  Built by Khoi Hoang https://github.com/khoih-prog/WebSockets_Generic
+  Licensed under MIT license
+
+  Originally Created on: 06.06.2016
+  Original Author: Markus Sattler
+*****************************************************************************************************************************/
+
+#if !defined(ESP8266)
+#error This code is intended to run only on the ESP8266 boards ! Please check your Tools->Board setting.
+#endif
+
+#define _WEBSOCKETS_LOGLEVEL_     3
+
+// For transport=websocket instead of transport=polling
+#define USING_STICKY_SESSION_SIO        true
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <ArduinoJson.h>
+
+#include <WebSocketsClient_Generic.h>
+#include <SocketIOclient_Generic.h>
+
+#include <Hash.h>
+
+ESP8266WiFiMulti WiFiMulti;
+SocketIOclient socketIO;
+
+// Select the IP address according to your local network
+IPAddress serverIP(192, 168, 2, 30);
+uint16_t  serverPort = 8080;    //3000;
+
+void socketIOEvent(socketIOmessageType_t type, uint8_t * payload, size_t length) 
+{
+  switch (type) 
+  {
+    case sIOtype_DISCONNECT:
+      Serial.println("[IOc] Disconnected");
+      break;
+    case sIOtype_CONNECT:
+      Serial.print("[IOc] Connected to url: ");
+      Serial.println((char*) payload);
+
+      // join default namespace (no auto join in Socket.IO V3)
+      socketIO.send(sIOtype_CONNECT, "/");
+      
+      break;
+    case sIOtype_EVENT:
+      Serial.print("[IOc] Get event: ");
+      Serial.println((char*) payload);
+      
+      break;
+    case sIOtype_ACK:
+      Serial.print("[IOc] Get ack: ");
+      Serial.println(length);
+      
+      hexdump(payload, length);
+      break;
+    case sIOtype_ERROR:
+      Serial.print("[IOc] Get error: ");
+      Serial.println(length);
+      
+      hexdump(payload, length);
+      break;
+    case sIOtype_BINARY_EVENT:
+      Serial.print("[IOc] Get binary: ");
+      Serial.println(length);
+      
+      hexdump(payload, length);
+      break;
+    case sIOtype_BINARY_ACK:
+       Serial.print("[IOc] Get binary ack: ");
+      Serial.println(length);
+      
+      hexdump(payload, length);
+      break;
+      
+    default:
+      break;  
+  }
+}
+
+void setup() 
+{
+  // Serial.begin(921600);
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.print("\nStart ESP8266_WebSocketClient_Sticky_SocketIO on "); Serial.println(ARDUINO_BOARD);
+  Serial.println(WEBSOCKETS_GENERIC_VERSION);
+
+  //Serial.setDebugOutput(true);
+
+  // disable AP
+  if (WiFi.getMode() & WIFI_AP) 
+  {
+    WiFi.softAPdisconnect(true);
+  }
+
+  WiFiMulti.addAP("SSID", "passpasspass");
+
+  //WiFi.disconnect();
+  while (WiFiMulti.run() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println();
+
+  // Client address
+  Serial.print("WebSockets Client started @ IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // server address, port and URL
+  Serial.print("Connecting to WebSockets Server @ IP address: ");
+  Serial.print(serverIP);
+  Serial.print(", port: ");
+  Serial.println(serverPort);
+
+  // setReconnectInterval to 10s, new from v2.5.1 to avoid flooding server. Default is 0.5s
+  socketIO.setReconnectInterval(10000);
+
+  socketIO.setExtraHeaders("Authorization: 1234567890");
+
+  // server address, port and URL
+  // void begin(IPAddress host, uint16_t port, String url = "/socket.io/?EIO=4", String protocol = "arduino");
+  // To use default EIO=4 fron v2.5.1
+  socketIO.begin(serverIP, serverPort);
+
+  // event handler
+  socketIO.onEvent(socketIOEvent);
+}
+
+unsigned long messageTimestamp = 0;
+
+void loop() 
+{
+  socketIO.loop();
+
+  uint64_t now = millis();
+
+  if (now - messageTimestamp > 30000) 
+  {
+    messageTimestamp = now;
+
+    // creat JSON message for Socket.IO (event)
+    DynamicJsonDocument doc(1024);
+    JsonArray array = doc.to<JsonArray>();
+
+    // add evnet name
+    // Hint: socket.on('event_name', ....
+    array.add("event_name");
+
+    // add payload (parameters) for the event
+    JsonObject param1 = array.createNestedObject();
+    param1["now"]     = (uint32_t) now;
+
+    // JSON to String (serializion)
+    String output;
+    serializeJson(doc, output);
+
+    // Send event
+    socketIO.sendEVENT(output);
+
+    // Print JSON for debugging
+    Serial.println(output);
+  }
+}
